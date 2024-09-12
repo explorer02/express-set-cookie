2fcd74d4f09e28b93eda61ffd309b0c0c6e79658
Ajay Bhardwaj <ajay.bhardwaj@ajay.bhardwaj-K14Y0XG40H>
2024-09-12 11:25:10 +0530

    endpoint

--- functions/app.js
+++ functions/app.js
@@ -2,11 +2,32 @@
 
 const express = require("express");
 const serverless = require("serverless-http");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const router = express.Router();
+app.use(cookieParser());
 
 router.get("/", (req, res) => {
   res.send("App is running..");
+});
+
+app.post("/api/data", (req, res) => {
+  // Set a cookie
+  res.cookie("myCookie", "cookieValue", {
+    httpOnly: true, // Makes the cookie inaccessible to JavaScript
+    secure: true, // Cookie will only be sent over HTTPS
+    maxAge: 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
+  });
+
+  // Return dummy JSON
+  res.json({
+    message: "Hello, world!",
+    data: {
+      key1: "value1",
+      key2: "value2",
+    },
+  });
 });
 
 app.use("/.netlify/functions/app", router);
