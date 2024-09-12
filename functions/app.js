9365bd1c964ce885f70cce2d29ae677019c179cf
Ajay Bhardwaj <ajay.bhardwaj@ajay.bhardwaj-K14Y0XG40H>
2024-09-12 13:30:37 +0530

    3

--- functions/app.js
+++ functions/app.js
@@ -11,19 +11,48 @@ app.use(cookieParser());
 app.use(cors());
 
 router.get("/", (req, res) => {
-  res.cookie("myCookie", "cookieValue", {
+  res.cookie("cookie1", "cookieValue", {
     httpOnly: true, // Makes the cookie inaccessible to JavaScript
     secure: true, // Cookie will only be sent over HTTPS
     maxAge: 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
   });
 
-  res.cookie("test_cookie", "test_value", {
+  res.cookie("cookie2", "test_value", {
     domain: ".sprinklr.com", // Allow cookie for subdomains
     path: "/",
-    sameSite: "None", // Cross-origin cookies
+    sameSite: "none", // Cross-origin cookies
     secure: true, // Cookies only sent over HTTPS
     httpOnly: true, // Cookies not accessible via JavaScript
     expires: new Date(Date.now() + 3600000), // Cookie expiration
+  });
+
+  res.cookie("cookie3", "test_value", {
+    domain: ".sprinklr.com", // Allow cookie for subdomains
+    path: "/",
+    sameSite: "none", // Cross-origin cookies
+    secure: true, // Cookies only sent over HTTPS
+    httpOnly: true, // Cookies not accessible via JavaScript
+    maxAge: 10 * 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
+    expires: new Date("20 sep 2024"),
+  });
+
+  res.cookie("cookie4", "test_value", {
+    domain: "*", // Allow cookie for subdomains
+    path: "/",
+    sameSite: "none", // Cross-origin cookies
+    secure: true, // Cookies only sent over HTTPS
+    httpOnly: true, // Cookies not accessible via JavaScript
+    maxAge: 10 * 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
+    expires: new Date("20 sep 2024"),
+  });
+
+  res.cookie("cookie5", "test_value", {
+    path: "/",
+    sameSite: "lax", // Cross-origin cookies
+    secure: true, // Cookies only sent over HTTPS
+    httpOnly: true, // Cookies not accessible via JavaScript
+    maxAge: 10 * 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
+    expires: new Date("20 sep 2024"),
   });
 
   res.json({ data: "App is running.." });
