8fbd325f94935527772cf8d9936a1f79bc4bcf35
Ajay Bhardwaj <ajay.bhardwaj@ajay.bhardwaj-K14Y0XG40H>
2024-09-12 13:06:59 +0530

    ss

--- functions/app.js
+++ functions/app.js
@@ -32,5 +32,23 @@ app.post("/api/data", (req, res) => {
   });
 });
 
+app.get("/api/data", (req, res) => {
+  // Set a cookie
+  res.cookie("myCookie", "cookieValue", {
+    httpOnly: true, // Makes the cookie inaccessible to JavaScript
+    secure: true, // Cookie will only be sent over HTTPS
+    maxAge: 24 * 60 * 60 * 1000, // Cookie expiration time (1 day)
+  });
+
+  // Return dummy JSON
+  res.json({
+    message: "Hello, world GET!",
+    data: {
+      key1: "value1",
+      key2: "value2",
+    },
+  });
+});
+
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
